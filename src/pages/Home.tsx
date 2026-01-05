fcf49e17a466c60a5623669f1c1d45001acd78f8
FranciscoLopezKanto <91705663+FranciscoLopezKanto@users.noreply.github.com>
2026-01-05 14:10:51 -0300

    Remove unused Link import from Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { motion } from 'framer-motion';
 import { LeaveCard } from '../components/LeaveCard';
 import { HomeMotion } from '../common/motion/Home';
-import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 export const Home = () => {
